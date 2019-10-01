94f76afbfc846d773969f4db254fe6c9fafcbf7b
Dima <wannafIy@protonmail.com>
2019-10-01 16:24:26 +0300

    Update permissions.py

--- core/permissions.py
+++ core/permissions.py
@@ -16,7 +16,6 @@ class UserCanUseVoiceTrack(BasePermission):
 	message = 'User cant get this track.'
 
 	def has_object_permission(self, request, view, obj):
-
 		user = request.user
 		if type(obj) is VoiceTrack:
 			return obj.user_have_permis(user)
