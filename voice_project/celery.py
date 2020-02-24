a4f62ad2a7a796fbc706c07d42ae89ca8b26d683
Dima <wannafIy@protonmail.com>
2020-02-25 00:05:49 +0300

    Update celery.py

--- voice_project/celery.py
+++ voice_project/celery.py
@@ -4,6 +4,8 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'voice_project.settings')
 
+
+app = Celery('voice')
 app = Celery('voice')
 app = Celery('voice')
 app.config_from_object('django.conf:settings', namespace='CELERY')
