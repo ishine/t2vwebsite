5855e624b6c627dd253b0e6431a3c6fbcaea1c97
Dima <wannafIy@protonmail.com>
2020-02-25 10:32:13 +0300

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from .models import CustomUser
 from captcha.fields import ReCaptchaField
 # thrhr
-
+#dsf
 class CustomUserCreationForm(UserCreationForm):
 
 	captcha = ReCaptchaField()
